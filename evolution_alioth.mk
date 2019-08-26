caada2d0c726898c187280f0a73ae40e63a53f98
minz1 <emerytang@gmail.com>
2019-08-26 09:42:19 -0500

    alioth: overlay: enable burn-in protection
    Change-Id: I66713222ea83c6ccee39dc49effc36d59546c93f
    Signed-off-by: sakhiofsakshi <imsakshisharma04@gmail.com>

---
+++ evolution_alioth.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
+TARGET_USES_BLUR := true
+#USE_PIXEL_CHARGING := true
+TARGET_BOOT_ANIMATION_RES := 1080
+#USE_AOSP_CLOCK := true
+PRODUCT_NAME := evolution_alioth
+PRODUCT_DEVICE := alioth
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F3
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.3.0.RKHMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHMIXM:user/release-keys
