ef33e5aeedf9becf77413b29807b9e2e4f6df04c
sakhiofsakshi <imsakshisharma04@gmail.com>
2021-12-24 03:54:32 +0000

    evolution bringup
    Signed-off-by: sakhiofsakshi <imsakshisharma04@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_alioth.mk
+    $(LOCAL_DIR)/evolution_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_alioth-user \
-    nad_alioth-userdebug \
-    nad_alioth-eng
+    evolution_alioth-user \
+    evolution_alioth-userdebug \
+    evolution_alioth-eng
