738d67d02817a0437c97218f44df8899d081e3c2
sakhiofsakshi <imsakshisharma04@gmail.com>
2022-01-14 08:03:24 +0000

    alioth:update fp
    Signed-off-by: sakhiofsakshi <imsakshisharma04@gmail.com>

--- cherish_alioth.mk
+++ cherish_alioth.mk
@@ -27,9 +27,9 @@ CHERISH_BUILD_TYPE := OFFICIAL
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.3.0.RKHMIXM release-keys"
+    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.5.0.RKHMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.3.0.RKHMIXM:user/release-keys
+BUILD_FINGERPRINT := Poco/alioth_global/alioth:11/RKQ1.200826.002/V12.5.5.0.RKHMIXM:user/release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.cherish.maintainer=hungphan2001&revolwoc
