c98a29e3d34c915c730451920ce90f3520d5e928
sakhiofsakshi <imsakshisharma04@gmail.com>
2022-01-15 12:04:09 +0000

    alioth:update to raven january fp
    "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
    
    Signed-off-by: sakhiofsakshi <imsakshisharma04@gmail.com>

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -9,7 +9,7 @@
 
 #include "vendor_init.h"
 
-#define FINGERPRINT "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
+#define FINGERPRINT "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
 
 static const variant_info_t aliothcn_info = {
     .hwc_value = "CN",
