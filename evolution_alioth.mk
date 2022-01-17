f0537c8db2c02ba8123704bbf8505c25729b6e27
SebaUbuntu <barezzisebastiano@gmail.com>
2022-01-17 21:58:05 +0530

    alioth: Update blobs from V12.5.4.0.RKHMIXM
    Signed-off-by: sakhiofsakshi <imsakshisharma04@gmail.com>

--- evolution_alioth.mk
+++ evolution_alioth.mk
@@ -23,10 +23,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.5.0.RKHMIXM release-keys"
+    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.4.0.RKHMIXM release-keys"
 
-BUILD_FINGERPRINT := Poco/alioth_global/alioth:11/RKQ1.200826.002/V12.5.5.0.RKHMIXM:user/release-keys
+BUILD_FINGERPRINT := Poco/alioth_global/alioth:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys
