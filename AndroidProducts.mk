af93cd586d9f96821486fa5e6940da07abedb7f0
revolwoc <imsakshisharma04@gmail.com>
2022-07-09 11:42:54 +0530

    welcome Cherish""

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/cherish_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    cherish_alioth-user \
+    cherish_alioth-userdebug \
+    cherish_alioth-eng
