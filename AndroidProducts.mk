07f71f7ada9690d5ddae7ddce305c9bc6b378495
johnmart19 <johnivan19999@gmail.com>
2022-07-22 12:49:46 +0300

    evolution
    Signed-off-by: revolwoc <imsakshisharma04@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2022 VoidUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/evolution_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    evolution_alioth-user \
+    evolution_alioth-userdebug \
+    evolution_alioth-eng
