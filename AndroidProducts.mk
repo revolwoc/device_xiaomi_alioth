a0ce712c2e9fc3a07593049774e522ffe529ea31
revolwoc <imsakshisharma04@gmail.com>
2022-12-24 17:09:19 +0000

    ppui
    Signed-off-by: revolwoc <imsakshisharma04@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_alioth.mk
+    $(LOCAL_DIR)/aosp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_alioth-user \
-    spark_alioth-userdebug \
-    spark_alioth-eng
+    aosp_alioth-user \
+    aosp_alioth-userdebug \
+    aosp_alioth-eng
--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,12 +8,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common spark stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := spark_alioth
+PRODUCT_NAME := aosp_alioth
 TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
@@ -29,3 +29,6 @@ TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.3.0.SKHMIXM:user/release-keys
+
+PPUI_MAINTAINER := revolwoc
+CUSTOM_BUILD_TYPE := OFFICIAL
